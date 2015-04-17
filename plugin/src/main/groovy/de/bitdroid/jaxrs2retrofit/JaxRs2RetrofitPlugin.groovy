6dbd9bc4503eac736838eeeeb396b3852f183f4c
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-04-17 17:21:11 +0200

    Plugin adds tasks for each android configuration

--- plugin/src/main/groovy/de/bitdroid/jaxrs2retrofit/JaxRs2RetrofitPlugin.groovy
+++ plugin/src/main/groovy/de/bitdroid/jaxrs2retrofit/JaxRs2RetrofitPlugin.groovy
@@ -1,4 +1,6 @@
 package de.bitdroid.jaxrs2retrofit
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
@@ -7,12 +9,29 @@ public final class JaxRs2RetrofitPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        def task = project.tasks.create('jaxRs2Retrofit', JaxRs2RetrofitTask)
+
         boolean hasJava = project.plugins.hasPlugin JavaPlugin
+        boolean hasAndroidApp = project.plugins.hasPlugin AppPlugin
+        boolean hasAndroidLib = project.plugins.hasPlugin LibraryPlugin
+
         if (hasJava) {
+            def task = project.tasks.create('jaxRs2Retrofit', JaxRs2RetrofitTask)
             project.compileJava.dependsOn task
             project.compileJava.source += task.outputs.files
         }
+
+        if (hasAndroidApp || hasAndroidLib) {
+            def variants
+            if (hasAndroidApp) variants = project.android.applicationVariants
+            else variants = project.android.libraryVariants
+
+            variants.all { variant ->
+                def task = project.tasks.create("jaxRs2Retrofit-${variant.name}", JaxRs2RetrofitTask)
+                variant.javaCompile.dependsOn task
+                variant.registerJavaGeneratingTask task, task.outputDir
+            }
+        }
+
     }
 
 }
