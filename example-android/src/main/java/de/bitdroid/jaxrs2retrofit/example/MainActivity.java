80d7b1de8fcb32af954fc676b8b14feea204648c
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-04-17 21:54:57 +0200

    Remove appcompat from example-android

--- example-android/src/main/java/de/bitdroid/jaxrs2retrofit/example/MainActivity.java
+++ example-android/src/main/java/de/bitdroid/jaxrs2retrofit/example/MainActivity.java
@@ -1,52 +1,22 @@
 package de.bitdroid.jaxrs2retrofit.example;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import retrofit.Callback;
 import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu_main, menu);
 
 		MyResource myResource = new RestAdapter.Builder()
 				.setEndpoint("http://example.com")
 				.build()
 				.create(MyResource.class);
-
-		myResource.getHelloWorld(new Callback<String>() {
-			@Override
-			public void success(String s, Response response) {
-				Log.d("", "hello world success");
-			}
-
-			@Override
-			public void failure(RetrofitError error) {
-				Log.d("", "hello world error");
-			}
-		});
-
-		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
-
-	}
 }
